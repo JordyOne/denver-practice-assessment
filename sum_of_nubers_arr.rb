b04b31a213f3a5dac9b7ea1ca89e7541288eb73f
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-06-23 10:08:34 -0600

    a slightly more effecient way of summing numbers arr

--- sum_of_nubers_arr.rb
+++ sum_of_nubers_arr.rb
@@ -1,8 +1,8 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
 
-number_sum = nil
+number_sum = nil.to_i
 numbers.each do |number|
-  number_sum = number_sum.to_i + number.to_i
+  number_sum = number_sum + number.to_i
 end
 
 print number_sum
